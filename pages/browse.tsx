d4a80e7518756b0492cb74c8ead2e67354e7a50f
Shilpa Kumari <145902859+Shilpakumari-12@users.noreply.github.com>
2025-01-13 17:35:12 +0530

    added navbar

--- pages/browse.tsx
+++ pages/browse.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @next/next/no-img-element */
+ /* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
